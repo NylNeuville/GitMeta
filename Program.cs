b2382e5c3ff159e697ab1fc6a195f5d773e77fa6
Nyl <95429032+NylNeuville@users.noreply.github.com>
2022-11-02 15:29:35 +0100

    Bitwise Algo Even Counter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int[] numbers = new int[5] { 1, 12, 6, 8, 10};
+
+static int BitwiseNumberOfEven(int[] numbers)
+{
+    int counter = 0;
+
+    for (var i = 0; i < numbers.Length; i++)
+    {
+        int add = numbers[i] & 1;
+        counter += (1 - add);
+    }
+
+    return counter;
+}
